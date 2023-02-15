0306ef40e13c800f65aff26209aa07c5956a8e14
Ricardo Duarte <rickduarte@gmail.com>
2023-02-15 18:15:12 -0300

    add styles and theme manager

--- tut/lib/main.dart
+++ tut/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'app/app.dart';
+import 'package:tut/app/app.dart';
 
 void main() {
   runApp(MyApp());
---
+++ tut/lib/presentation/theme_manager.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:tut/presentation/color_manager.dart';
+import 'package:tut/presentation/font_manager.dart';
+import 'package:tut/presentation/styles_manager.dart';
+import 'package:tut/presentation/values_manager.dart';
+
+ThemeData getApplicationTheme() {
+  return ThemeData(
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.primaryOpacity70,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
+    accentColor: ColorManager.grey,
+    splashColor: ColorManager.primaryOpacity70,
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s4,
+    ),
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.primaryOpacity70,
+      titleTextStyle: getRegularStyle(
+        color: ColorManager.white,
+        fontSize: FontSize.s16,
+      ),
+    ),
+  );
+}
