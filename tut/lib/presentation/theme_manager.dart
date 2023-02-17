0fd0b2713078ef075f92a8ec402a0cc0aa6e7882
Ricardo Duarte <rickduarte@gmail.com>
2023-02-17 13:57:16 -0300

    finish implementing themes for the app

--- tut/lib/presentation/theme_manager.dart
+++ tut/lib/presentation/theme_manager.dart
@@ -10,7 +10,9 @@ ThemeData getApplicationTheme() {
     primaryColorLight: ColorManager.primaryOpacity70,
     primaryColorDark: ColorManager.darkPrimary,
     disabledColor: ColorManager.grey1,
-    accentColor: ColorManager.grey,
+    colorScheme: ColorScheme.fromSwatch().copyWith(
+      secondary: ColorManager.grey,
+    ),
     splashColor: ColorManager.primaryOpacity70,
     cardTheme: CardTheme(
       color: ColorManager.white,
@@ -36,8 +38,39 @@ ThemeData getApplicationTheme() {
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         textStyle: getRegularStyle(color: ColorManager.white),
-        primary: ColorManager.primary,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(AppSize.s12)),
+        backgroundColor: ColorManager.primary,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(AppSize.s12)),
+      ),
+    ),
+    textTheme: TextTheme(
+      displayLarge: getSemiBoldStyle(
+          color: ColorManager.darkGrey, fontSize: FontSize.s16),
+      titleMedium:
+          getMediumStyle(color: ColorManager.lightGrey, fontSize: FontSize.s14),
+      labelLarge: getRegularStyle(color: ColorManager.grey1),
+      bodyLarge: getRegularStyle(color: ColorManager.grey),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: EdgeInsets.all(AppPadding.p8),
+      hintStyle: getRegularStyle(color: ColorManager.grey1),
+      labelStyle: getMediumStyle(color: ColorManager.darkGrey),
+      errorStyle: getRegularStyle(color: ColorManager.error),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: ColorManager.grey, width: AppSize.s1_5),
+        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8)),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: ColorManager.primary, width: AppSize.s1_5),
+        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8)),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: ColorManager.error, width: AppSize.s1_5),
+        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8)),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: ColorManager.primary, width: AppSize.s1_5),
+        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8)),
       ),
     ),
   );
